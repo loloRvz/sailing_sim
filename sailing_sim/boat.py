ba255ff3908427b42d97e0a9a27b217cfafaef38
Laurent Rouvinez <rouvinez.laurent@gmail.com>
2024-12-25 18:10:56 +0100

    typos

--- sailing_sim/boat.py
+++ sailing_sim/boat.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-from config import W_HALF, H_HALF, BOAT_PARAMS
+import config as cfg
 
 class Inputs():
     up = False
@@ -18,7 +18,7 @@ class Boat():
     rot_vel = 0
     rot_acc = 0
 
-    shape = BOAT_PARAMS.shape
+    shape = cfg.BOAT_shape
 
     inputs = Inputs()
 
@@ -31,42 +31,41 @@ class Boat():
         force = np.zeros(2)
         torque = 0
 
-        # Keel force (side force)
+        # Compute longitudinal and lateral velocity components
         rot_vec = np.array([math.cos(self.rot), math.sin(self.rot)])
         abs_long_vel = np.dot(self.vel, rot_vec)
         long_vel = abs_long_vel * rot_vec
         lat_vel = self.vel - long_vel
 
         # Motor force (front and back)
-        force += BOAT_PARAMS.F * (self.inputs.up - self.inputs.down) * rot_vec
+        force += cfg.BOAT_F * (self.inputs.up - self.inputs.down) * rot_vec
 
         # Rudder force (rotation)
-        torque += BOAT_PARAMS.R * abs_long_vel * (self.inputs.left - self.inputs.right)
+        torque += cfg.BOAT_R * abs_long_vel * (self.inputs.left - self.inputs.right)
 
         # Keel force (lateral)
-        force += BOAT_PARAMS.K * -lat_vel
+        force += cfg.BOAT_K * -lat_vel
         
 
         # F = ma
-        self.acc = 1/BOAT_PARAMS.M * (force - BOAT_PARAMS.b * self.vel)
+        self.acc = 1/cfg.BOAT_M * (force - cfg.BOAT_b * self.vel)
         self.vel = self.vel + self.acc * self.dt
         self.pos = self.pos + self.vel * self.dt
 
         # T = Jw'
-        self.rot_acc = 1/BOAT_PARAMS.J * (torque - BOAT_PARAMS.v * self.rot_vel)
+        self.rot_acc = 1/cfg.BOAT_J * (torque - cfg.BOAT_v * self.rot_vel)
         self.rot_vel += self.rot_acc * self.dt
         self.rot += self.rot_vel * self.dt
 
 
-
         # Loop back boat position if out of screen
-        if self.pos[0] > W_HALF:
-            self.pos[0] = -W_HALF
-        if self.pos[0] < -W_HALF:
-            self.pos[0] = W_HALF
-        if self.pos[1] > H_HALF:
-            self.pos[1] = -H_HALF
-        if self.pos[1] < -H_HALF:
-            self.pos[1] = H_HALF
+        if self.pos[0] > cfg.W_HALF:
+            self.pos[0] = -cfg.W_HALF
+        if self.pos[0] < -cfg.W_HALF:
+            self.pos[0] = cfg.W_HALF
+        if self.pos[1] > cfg.H_HALF:
+            self.pos[1] = -cfg.H_HALF
+        if self.pos[1] < -cfg.H_HALF:
+            self.pos[1] = cfg.H_HALF
 
 
